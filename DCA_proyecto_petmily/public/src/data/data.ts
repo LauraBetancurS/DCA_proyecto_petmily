4ba22a9ff6969297fb932f26bb9514235ca38630
LauraBetancurS <lau1betancur1@gmail.com>
2024-09-24 18:17:18 -0500

    Add data creation functionality

---
+++ DCA_proyecto_petmily/public/src/data/data.ts
@@ -0,0 +1,50 @@
+export const data = [
+    {
+        uid: 1,
+        username: 'doglover99',
+        name: 'Max',
+        profileImg: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpfp1.png?alt=media&token=a288411a-eeb0-46b3-adfc-9db0d3bb6fb6',
+        profileDesc: 'Amante de los paseos largos y los huesos.',
+        likes: 200,
+        comments: 40,
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost1.png?alt=media&token=dummytoken1',
+        postDesc: 'Disfrutando de un día soleado en el parque con mi pelota favorita!',
+        profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
+    },
+    {
+        uid: 2,
+        username: 'catqueen',
+        name: 'Luna',
+        profileImg: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpfp2.png?alt=media&token=0e025e0e-9a1a-475b-936e-379a21a26b6e',
+        profileDesc: 'Dueña del sofá y del corazón de todos.',
+        likes: 150,
+        comments: 30,
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost2.png?alt=media&token=dummytoken2',
+        postDesc: 'Aquí, posando en mi lugar favorito para dormir. ¡Nada como una buena siesta!',
+        profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
+    },
+    {
+        uid: 3,
+        username: 'furryadventures',
+        name: 'Bella',
+        profileImg: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpfp3.png?alt=media&token=0be8fb12-92d9-4c82-b3fb-c68a42ee8447',
+        profileDesc: 'Explorando el mundo, una aventura a la vez.',
+        likes: 220,
+        comments: 50,
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost3.png?alt=media&token=dummytoken3',
+        postDesc: 'Descubriendo nuevos senderos y olfateando todo lo que encuentro. ¡Amo la naturaleza!',
+        profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
+    },
+    {
+        uid: 4,
+        username: 'purrfectlyme',
+        name: 'Simba',
+        profileImg: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpfp4.png?alt=media&token=25b005c1-8042-4123-8791-2fad90606e3d',
+        profileDesc: 'Amo mis siestas y el sol.',
+        likes: 180,
+        comments: 35,
+        imgPost: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2Fpost4.png?alt=media&token=dummytoken4',
+        postDesc: '¡El lugar perfecto para una siesta bajo el sol! No me molesten.',
+        profileBanner: 'https://firebasestorage.googleapis.com/v0/b/narracion-hipermedia.appspot.com/o/imgs%2FLaura%20Betancur%2FBANNER.png?alt=media&token=e6bbcb47-cff7-4f83-a58a-00b1e958575f'
+    }
+];
